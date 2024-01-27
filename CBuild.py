289d4a9523f73e1084e49cbb9c9aba362d47ca7f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-27 02:06:06 +0100

    learn_DSA

--- CBuild.py
+++ CBuild.py
@@ -1,7 +1,18 @@
 import sys
 
 
-if not sys.argv:
-    print("you have to pass a script")
-else:
-    print(sys.argv)
+def check_script():
+    if len(sys.argv) > 2:
+         print(f"Error: Usage: {sys.argv[0]} <name of script>")
+    elif len(sys.argv) != 2:
+        print("you have to pass a script")
+    else:
+        print(sys.argv)
+
+
+
+#open the file and write into it.
+
+
+if __name__ == "__main__":
+    check_script()
